4bb0b932b4aad3d6da1ea5fd708d2f988b8c5725
srodr21 <128683260+srodr21@users.noreply.github.com>
2024-02-22 15:39:36 +0700

    postgres db config added

--- config/database.js
+++ config/database.js
@@ -1,11 +1,28 @@
-const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'database/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USER", "test"),
+      password: env("DATABASE_PASSWORD", "test"),
+      schema: env("DATABASE_SCHEMA", "public"),
+      ssl: true
     },
     useNullAsDefault: true,
   },
 });
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'database/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
